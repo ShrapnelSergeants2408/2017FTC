804bf403cbd06d3d77a101c928a8d938bd2a0f0f
JoshKupka <josh.kupka@gmail.com>
2017-09-11 10:31:52 -0500

    Create BasicBuild.java

---
+++ BasicBuild.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class BasicBuild extends OpMode {
+
+
+
+    @Override
+    public void init() {
+
+    }
+
+    /*
+       * Code to run when the op mode is first enabled goes here
+       * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+       */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * This method will be called ONCE when start is pressed
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+     */
+    @Override
+    public void start() {
+
+    }
+
+    /*
+     * This method will be called repeatedly in a loop
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+     */
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+    }
+}
+
+
