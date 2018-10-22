9484a18ba8428280ce5ed0962ae29adaebbdb800
Josh Kupka <josh.kupka@gmail.com>
2018-10-22 16:08:09 -0500

    Create AutoClassOneBlue.java

---
+++ AutoClassOneBlue.java
@@ -0,0 +1,108 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcontroller.internal.FTCBOTHARDWARE;
+
+@Autonomous (name = "AutoClassOneBlue", group = "Default")
+public class AutoClassOneBlue extends LinearOpMode {
+
+    FTCBOTHARDWARE robot = new FTCBOTHARDWARE();
+
+    boolean red = false;
+    boolean blue = true;
+
+    private final double driveforward = 0.5;
+    private final double shiftMove = 1.0;
+    private final double stop = 0.0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        int count = 0;
+        while (opModeIsActive()) {
+            switch (count) {
+                /*case 1:
+                    robot.Shift.setPower(-shiftMove);
+                    sleep(350);
+                    robot.Lift.setPower(driveforward);
+                    sleep(500);
+                    robot.Lift.setPower(stop);
+                    count++;
+                    break;*/
+                case 0:
+                    if (red && !blue) {
+                        robot.FrontLeft.setPower(-driveforward);
+                        robot.FrontRight.setPower(-driveforward);
+                        robot.BackLeft.setPower(-driveforward);
+                        robot.BackRight.setPower(-driveforward);
+                        sleep(1500);
+                        /*robot.FrontLeft.setPower(-driveforward);
+                        robot.FrontRight.setPower(driveforward);
+                        robot.BackLeft.setPower(-driveforward);
+                        robot.BackRight.setPower(driveforward);
+                        sleep(750);
+                        robot.FrontLeft.setPower(-driveforward);
+                        robot.FrontRight.setPower(-driveforward);
+                        robot.BackLeft.setPower(-driveforward);
+                        robot.BackRight.setPower(-driveforward);
+                        sleep(200);*/
+                        robot.FrontLeft.setPower(stop);
+                        robot.FrontRight.setPower(stop);
+                        robot.BackLeft.setPower(stop);
+                        robot.BackRight.setPower(stop);
+                        count++;
+                    } else if (blue && !red) {
+                        robot.FrontLeft.setPower(driveforward);
+                        robot.FrontRight.setPower(-driveforward);
+                        robot.BackLeft.setPower(driveforward);
+                        robot.BackRight.setPower(-driveforward);
+                        sleep(1500);
+                        /*robot.FrontLeft.setPower(driveforward);
+                        robot.FrontRight.setPower(-driveforward);
+                        robot.BackLeft.setPower(driveforward);
+                        robot.BackRight.setPower(-driveforward);
+                        sleep(750);
+                        robot.FrontLeft.setPower(-driveforward);
+                        robot.FrontRight.setPower(-driveforward);
+                        robot.BackLeft.setPower(-driveforward);
+                        robot.BackRight.setPower(-driveforward);
+                        sleep(200);*/
+                        robot.FrontLeft.setPower(stop);
+                        robot.FrontRight.setPower(stop);
+                        robot.BackLeft.setPower(stop);
+                        robot.BackRight.setPower(stop);
+                        count++;
+                    } else {
+                        robot.FrontLeft.setPower(stop);
+                        robot.FrontRight.setPower(stop);
+                        robot.BackLeft.setPower(stop);
+                        robot.BackRight.setPower(stop);
+                        count++;
+                    }
+                    break;
+                case 1:
+                    robot.FrontLeft.setPower(stop);
+                    robot.FrontRight.setPower(stop);
+                    robot.BackLeft.setPower(stop);
+                    robot.BackRight.setPower(stop);
+                    idle();
+                    break;
+                /*case 1:
+                    robot.Shift.setPower(shiftMove);
+                    sleep(350);
+                    robot.Shift.setPower(stop);
+                    idle();
+                    break;
+                default:
+                    break;*/
+            }
+            idle();
+        }
+    }
+}
